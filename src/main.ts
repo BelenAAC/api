8177437888256bb00545265f5646c1820d80aec1
BelenAAC <135270107+BelenAAC@users.noreply.github.com>
2024-01-13 02:01:17 -0300

    conexion apolloServer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ApolloServer } from '@apollo/server';
+import { GraphQLSchema, buildSchema } from 'graphql';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+ 
   await app.listen(3000);
 }
 bootstrap();
+
+
